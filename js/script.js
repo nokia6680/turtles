4dcaedaaea218efd0ee0a6d3ef15cd685c079852
Artur Ballod <artur.ballod@gmail.com>
2018-08-26 09:30:07 +0100

    #promo-tmnt

--- js/script.js
+++ js/script.js
@@ -48,3 +48,17 @@ var swiper = new Swiper('.swiper-container-1', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+var swiper = new Swiper('.swiper-container-2', {
+  touchReleaseOnEdges: 'true',
+  spaceBetween: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: 'true',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
